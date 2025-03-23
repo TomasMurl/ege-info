a3117a0d40cf3335a3c835bbaabe97f882653c5a
TomasMurl <tomasmurlychenko@mail.ru>
2025-03-23 17:05:29 +0700

    23.03

--- 25_6789.py
+++ 25_6789.py
@@ -1,5 +1,5 @@
 import re
 c = 0
-for i in range(123405, 10 ** 8):
-    if re.fullmatch(r"12[\d]*34\d5", str(i)) and i % 2025 == 0:
+for i in range(0, 10 ** 8, 2025):
+    if re.fullmatch("12[\d]*34\d5", str(i)):
         print(i, i // 2025)
