95909c327ebeca4b483d41801d40663cfbe4ca0b
TomasMurl <tomasmurlychenko@mail.ru>
2025-06-07 18:22:13 +0700

    07.06

--- Seva/25_21422.py
+++ Seva/25_21422.py
@@ -6,16 +6,15 @@ def get_dels(n):
             dels.add(n // i)
     return sorted(dels)
 
-def get_min_del(dels):
-    for d in dels:
-        if str(d)[-1] == "7" and d != 7 and d != n:
-            return d
-
 c = 0
 for n in range(1125000, 2000000):
     dels = get_dels(n)
-    min_del = get_min_del(dels)
-    if min_del != None:
+    min_del = 0
+    for d in dels:
+        if str(d)[-1] == "7" and d != 7 and d != n:
+            min_del = d
+            break
+    if min_del != 0:
         c += 1
         print(n, min_del)
     if c == 5:
