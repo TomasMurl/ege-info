c9ebfff59ff854189d5297ad50bdb8dbd4ed4975
TomasMurl <tomasmurlychenko@mail.ru>
2025-06-10 21:41:30 +0700

    10.06

--- Nikita/prognoz/25.py
+++ Nikita/prognoz/25.py
@@ -9,7 +9,9 @@ def get_dels(n):
 c = 0
 for n in range(2_900_000, 10_000_000):
     dels = get_dels(n)
-    if len(dels) == 4: # Если делителей больше 4, то значит там не только 2 простых делителя
+    len_dels_1 = len(get_dels(dels[1]))
+    len_dels_2 = len(get_dels(dels[2]))
+    if len(dels) == 4 and len_dels_1 == 2 and len_dels_2 == 2: про
         if str(dels[1]).count('0') == 1 and str(dels[2]).count('0') == 1: # проверяем, что в каждом делителе у нас только один "0"
             c += 1
             print(n, dels[2])
---
+++ test.py
@@ -0,0 +1,6 @@
+from itertools import *
+otrezki = combinations(range(0, 1000), 2)
+
+for A in otrezki:
+    S = range(A[0], A[1]+1)
+    x in S
