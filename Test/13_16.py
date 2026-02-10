29c1fbd883997bcb5ecf7db867dd47ea94481773
TomasMurl <tomasmurlychenko@mail.ru>
2026-02-11 00:00:24 +0800

    11.02

--- Test/13_16.py
+++ Test/13_16.py
@@ -1,9 +1,2 @@
-import ipaddress
+import itertools
 
-target_ip = ipaddress.ip_address("111.81.208.27")
-target_net = ipaddress.ip_address("111.81.192.0")
-for mask in range(1, 33):
-    net = ipaddress.ip_network(f"111.81.192.0/{mask}", 0)
-    if target_ip in net and net.network_address == target_net:
-        mask = ipaddress.ip_network(f"0.0.0.0/{mask}").netmask
-        print(mask)
