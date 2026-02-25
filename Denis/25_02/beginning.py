ab0cf22d89034a6129c6c7923dcc331ee3727f40
TomasMurl <tomasmurlychenko@mail.ru>
2026-02-25 21:01:13 +0700

    25.02

---
+++ Denis/25_02/beginning.py
@@ -0,0 +1,35 @@
+# IDE - среда разработки
+
+# + - * / // % - операторы
+# a, b, c - операнды
+a = 100
+b = a + a
+c = a + b * 2
+a = 200
+a = a + 5
+
+# Типы данных в Python
+# int() - integer - целочисленный тип данных
+
+a = 5
+b = 10
+# print(a + b, a - b, a * b, a / b)
+# print(17 // 4, 17 % 4)
+
+# str() - string - строчный тип данных
+
+a = "swaa"
+b = '123'
+c = a + b
+# Конкатенация
+# print(c + "llslslsl")
+# Метод - функция, которая есть у объекта
+a = "swadakwjdlksddjklckjlqhdfkljalashkj"
+# print( a.count("j"), len(a) )
+
+# Кастинг типов данных / Изменение типов данных
+a = 23
+print(a, type(a))
+a = str(a)
+print(a, type(a))
+print(int("123"))
