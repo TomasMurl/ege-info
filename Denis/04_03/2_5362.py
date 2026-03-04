e707e6209402ea5da51e1ad75075fc580bc443d2
TomasMurl <tomasmurlychenko@mail.ru>
2026-03-04 20:57:38 +0700

    04.03

---
+++ Denis/04_03/2_5362.py
@@ -0,0 +1,14 @@
+# стрелка вправо - <=
+# стрелка вниз - or
+# стрелка вверх - and
+# полочка - not
+# тройное равно - ==
+
+print("x y z w F")
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = ((x <= z) <= y) or (not w)
+                if F == 0:
+                    print(x, y, z, w, 0)
---
+++ Denis/04_03/beginning.py
@@ -0,0 +1,29 @@
+## for - Цикл
+
+a = [10, 20, 30, 40, 50]
+b = 34
+c = "1231"
+
+# for i in a:
+#     print("Hello")
+
+# range(5) = [0, 1, 2, 3, 4]
+# range(10) = [0, 1, ..., 9]
+# range(1, 5) = [1, 2, 3, 4]
+# range(5, 10) = [5, 6, 7, 8, 9]
+# range(1, 10, 2) = [1, 3, 5, 7, 9]
+
+# for i in range(5):
+#     print("Qwe")
+
+# for N in range(10):
+#     p = N ** 2 + 1
+#     print(p)
+#     if p > 10:
+#         print("Больше!")
+#     else:
+#         print("Меньше!")
+
+# for i in range(5):
+#     for j in range(5):
+#         print(i, j)
