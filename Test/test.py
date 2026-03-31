f69dbf0d1d2912671a6b63be934e1148c9cae21b
TomasMurl <tomasmurlychenko@mail.ru>
2026-03-31 20:56:50 +0700

    30.03

--- Test/test.py
+++ Test/test.py
@@ -1,31 +1,13 @@
-# # 2
-# while True:
-#     s = input()
-#     if s == '0':
-#         break
-#     print(len(s))
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r = str(n % b) + r
+        n = n // b
+    return r
 
-# # 4
-# n = int(input())
-# h = 12
-# for i in range(h, n + h):
-#     print(str(i % 24) + ':00')
-
-# # 5
-# r = ''
-# while True:
-#     s = input()
-#     if s == '0':
-#         break
-#     if r == '':
-#         r = s
-#     else:
-#         r = s + ' ' + r
-# print(r)
-
-# 3
-s = 0
-for i in range(1, 1000001):
-    if i % 5 == 0:
-        s = s + i
-print(s)
+for x in range(2030, 0, -1):
+    s = 7 ** 170 + 7 ** 100 - x
+    s_7 = convert(s, 7)
+    if s_7.count('0') == 71:
+        print(x)
+        break
