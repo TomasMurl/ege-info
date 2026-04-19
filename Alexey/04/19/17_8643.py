0c82c098fe7d3f10bfa16c809398533a60081d92
TomasMurl <tomasmurlychenko@mail.ru>
2026-04-19 16:58:46 +0700

    19.04

--- Alexey/04/19/17_8643.py
+++ Alexey/04/19/17_8643.py
@@ -10,7 +10,6 @@ for i in range(len(m) - 2):
     a, b, c = m[i], m[i + 1], m[i + 2]
     if all(len(str(abs(x))) != 4 for x in [a, b, c]) \
             and a + b + c > max_30:
-        print(a, b, c, a + b + c)
         cnt += 1
         max_sum = max(max_sum, a + b + c)
 print(cnt, max_sum)
