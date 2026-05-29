310a5d45a1301e1ece909a38db90a7adb324314c
TomasMurl <tomasmurlychenko@mail.ru>
2026-05-29 20:01:06 +0700

    28.05

---
+++ Maxim/05/28/14_8989.py
@@ -0,0 +1,14 @@
+def convert(n, b):
+    r = []
+    while n > 0:
+        r.append(n % b)
+        n = n // b
+    return r[::-1]
+
+min_0 = 100000000000000
+for x in range(1, 2031):
+    s = 6 ** 2030 + 6 ** 100 - x
+    s_6 = convert(s, 6)
+    if s_6.count(0) < min_0:
+        min_0 = s_6.count(0)
+print(min_0)
